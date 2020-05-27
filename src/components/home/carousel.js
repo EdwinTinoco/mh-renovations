eeb75251eb195d659d67cfa6a6df2afefe0a76c2
EdwinTinoco <jaredlotic@gmail.com>
2020-05-27 17:16:17 -0600

    added pictures to carousel

--- src/components/home/carousel.js
+++ src/components/home/carousel.js
@@ -1,25 +1,28 @@
 import React from "react";
 import { Carousel } from 'react-responsive-carousel';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
-import Img2 from "../../../public/img2.jpg"
+import Img1 from "./carousel_img/img1.jpg"
+import Img2 from "./carousel_img/img2.jpg"
+import Img3 from "./carousel_img/img3.jpg"
 
 export default function CarouselComponent() {
    return (
-      <div class="carousel-wrapper">
+      <div className="carousel-wrapper">
          <Carousel
             infiniteLoop
             useKeyboardArrows
             autoPlay
             showStatus={false}
+            showThumbs={false}
          >
             <div>
-               <img src="../../../public/img2.jpg" />
+               <img src={Img1} />
             </div>
             <div>
-               <img src="../../../public/img2.jpg" />
+               <img src={Img2} />
             </div>
             <div>
-               <img src="../../../public/img2.jpg" />
+               <img src={Img3} />
             </div>
          </Carousel>
       </div>
