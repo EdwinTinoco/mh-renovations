9a3ae71e8369255e9b8a6e6e4cd10fe91d2cd8bc
EdwinTinoco <jaredlotic@gmail.com>
2020-05-28 09:12:33 -0600

    added pictures to carousel folder

--- src/components/home/carousel.js
+++ src/components/home/carousel.js
@@ -4,6 +4,8 @@ import "react-responsive-carousel/lib/styles/carousel.min.css";
 import Img1 from "./carousel_img/img1.jpg"
 import Img2 from "./carousel_img/img2.jpg"
 import Img3 from "./carousel_img/img3.jpg"
+import Img4 from "./carousel_img/img4.jpg"
+import Img6 from "./carousel_img/img6.jpg"
 
 export default function CarouselComponent() {
    return (
@@ -24,6 +26,13 @@ export default function CarouselComponent() {
             <div>
                <img src={Img3} />
             </div>
+            <div>
+               <img src={Img4} />
+            </div>
+            <div>
+               <img src={Img6} />
+            </div>
+
          </Carousel>
       </div>
    );
