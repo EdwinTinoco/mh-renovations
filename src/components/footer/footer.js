e5d17d5f3ed7004927381990df0af6436d0a1a28
EdwinTinoco <jaredlotic@gmail.com>
2020-05-29 07:19:35 -0600

    added styles

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -5,68 +5,77 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 export default function Footer() {
    return (
       <div className="footer-main-wrapper">
-         <div className="right-column">
-            <div className="title">
-               <h2>About Us</h2>
-            </div>
-            <div className="description">
-               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed a felis
-               non sem elementum tempor in at urna. Suspendisse auctor libero ut nibh
-               consequat sed sagittis dolor iaculis. Donec condimentum mauris nec eros
+         <div className="footer-content">
+            <div className="right-column">
+               <div className="title">
+                  <h2>About Us</h2>
+               </div>
+               <div className="description">
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed a felis
+                  non sem elementum tempor in at urna. Suspendisse auctor libero ut nibh
+                  consequat sed sagittis dolor iaculis. Donec condimentum mauris nec eros
                auctor sed vestibulum tellus consequat.</p>
+               </div>
+            </div>
+
+            <div className="center-column">
+               <div className="title">
+                  <h2>Our Services</h2>
+               </div>
+               <div className="links-wrapper">
+                  <div className="link">
+                     <Link to="/">Planning & Design</Link>
+                  </div>
+                  <div className="link">
+                     <Link to="/">Project & Management</Link>
+                  </div>
+                  <div className="link">
+                     <Link to="/">Kitchen Renovations</Link>
+                  </div>
+                  <div className="link">
+                     <Link to="/">Bathrooms Renovations</Link>
+                  </div>
+               </div>
+            </div>
+
+            <div className="left-column">
+               <div className="title">
+                  <h2>Get in touch</h2>
+               </div>
+
+               <div className="phone-number-icon">
+                  <div className="icon">
+                     <FontAwesomeIcon icon="phone" />
+                  </div>
+                  <div className="phone">
+                     555 555-5555
+               </div>
+               </div>
+
+               <div className="email-icon">
+                  <div className="icon">
+                     <FontAwesomeIcon icon="envelope" />
+                  </div>
+                  <div className="email">
+                     ejemplo@gmail.com
+               </div>
+               </div>
+
+               <div className="social-media">
+                  <div className="facebook">
+                     facebook
+               </div>
+                  <div className="instagram">
+                     instagram
+               </div>
+               </div>
             </div>
          </div>
 
-         <div className="center-column">
-            <div className="title">
-               <h2>Our Services</h2>
-            </div>
-            <div className="links-wrapper">
-               <div className="link">
-                  <Link to="/">Planning & Design</Link>
-               </div>
-               <div className="link">
-                  <Link to="/">Project & Management</Link>
-               </div>
-               <div className="link">
-                  <Link to="/">Kitchen Renovations</Link>
-               </div>
-               <div className="link">
-                  <Link to="/">Bathrooms Renovations</Link>
-               </div>
-            </div>
-         </div>
-
-         <div className="left-column">
-            <div className="title">
-               <h2>Get in touch</h2>
-            </div>
-
-            <div className="phone-number-icon">
-               <div className="icon">
-                  <FontAwesomeIcon icon="phone" />
-               </div>
-               <div className="phone">
-                  555 555-5555
-               </div>
-            </div>
-
-            <div className="email-icon">
-               <div className="icon">
-                  <FontAwesomeIcon icon="envelope" />
-               </div>
-               <div className="email">
-                  ejemplo@gmail.com
-               </div>
-            </div>
-
-            <div className="social-media">
-               <div className="facebook">
-                  facebook
-               </div>
-               <div className="instagram">
-                  instagram
-               </div>
+         <div className="copyright-wrapper">
+            <hr />
+            <div className="info">
+               &copy; 2020 M&H Renovations &#124; All rights reserved &#124; Webmoun Co.
             </div>
          </div>
       </div>
