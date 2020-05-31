a69f7e7c991a9a080be8433648afc61ecc63429c
EdwinTinoco <jaredlotic@gmail.com>
2020-05-31 09:18:06 -0600

    Initial commit

--- src/components/app.js
+++ src/components/app.js
@@ -2,8 +2,10 @@ import React, { Component } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Icons from "../helpers/icons";
 
-import Navbar from "./navBar/navBar.js";
+import Navbar from "./navBar/navBar";
 import Home from "./pages/home";
+import ContactUs from "./pages/contact";
+import GetQuote from "./pages/get-quote";
 import Footer from "./footer/footer";
 
 export default class App extends Component {
@@ -20,6 +22,8 @@ export default class App extends Component {
           <Navbar />
           <Switch>
             <Route exact path="/" component={Home} />
+            <Route path="/contact" component={ContactUs} />
+            <Route path="/get-quote" component={GetQuote} />
           </Switch>
           <Footer />
         </Router>
