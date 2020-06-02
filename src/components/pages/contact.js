b7479646956263d39cc89315640ad71183ea3ce6
EdwinTinoco <jaredlotic@gmail.com>
2020-06-01 21:57:46 -0600

    added styles contact page

--- src/components/pages/contact.js
+++ src/components/pages/contact.js
@@ -10,43 +10,43 @@ export default function ContactUs() {
                <FontAwesomeIcon icon="phone" />
             </div>
             <div className="title">
-               <h1>Contact Us</h1>
+               <h1>Contact us</h1>
             </div>
          </div>
          <div className="form-info-wrapper">
             <div className="form-wrapper">
                <div className="title">
-                  <h2>We'd love to hear from You</h2>
+                  <h1>We'd love to hear from You</h1>
                   <p>If you have any questions or would like to book
                      an appointment fill out the form below or give us a call.</p>
                </div>
-               <div className="form">
-                  <form className="form">
-                     <div className="form-group">
-                        <input type="text" id="name" placeholder="Your name" />
-                        <label for="name">Your name</label>
-                     </div>
+               {/* <div className="form"> */}
+               <form className="form">
+                  <div className="form-group">
+                     <input type="text" id="name" placeholder="Your name" />
+                     <label for="name">Your name</label>
+                  </div>
 
-                     <div className="form-group">
-                        <input type="text" id="email" placeholder="Your email" />
-                        <label for="name">Your email</label>
-                     </div>
+                  <div className="form-group">
+                     <input type="text" id="email" placeholder="Your email" />
+                     <label for="name">Your email</label>
+                  </div>
 
-                     <div className="form-group">
-                        <input type="text" id="name" placeholder="Your phone" />
-                        <label for="name">Your phone</label>
-                     </div>
+                  <div className="form-group">
+                     <input type="text" id="name" placeholder="Your phone" />
+                     <label for="name">Your phone</label>
+                  </div>
 
-                     <div className="form-group">
-                        <textarea name="message" id="message" placeholder="Message" />
-                        <label for="message">Message</label>
-                     </div>
+                  <div className="form-group">
+                     <textarea name="message" id="message" placeholder="Message" />
+                     <label for="message">Message</label>
+                  </div>
 
-                     <div className="center-btn-wrapper">
-                        <button type="submit" class="btn">Send</button>
-                     </div>
-                  </form>
-               </div>
+                  <div className="center-btn-wrapper">
+                     <button type="submit" class="btn">Send</button>
+                  </div>
+               </form>
+               {/* </div> */}
             </div>
 
             <div className="info-wrapper">
@@ -68,14 +68,20 @@ export default function ContactUs() {
                <div className="phone-number">
                   <p>(555) 555-5555</p>
                   <p>(555) 555-5555</p>
+               </div>
+
+               <div className="email">
                   <p>ejemplo@gmail.com</p>
                </div>
 
                <div className="get-quote">
                   <p>Looking To Get A Quote On Your Next Project?</p>
                   <button type="button">
-                     <FontAwesomeIcon icon="calculator" />
-                     Get a quote</button>
+                     <div className="icon">
+                        <FontAwesomeIcon icon="calculator" />
+                     </div>
+                     Get a quote
+                  </button>
                </div>
             </div>
          </div>
