bfe17504265255f11c893517f52c2d7054148cad
Saul Vega <saulv@MacBook-Pro.local>
2020-06-04 13:50:26 -0600

    added aboutUs.js file and aboutUs.scss file, also added to navbar

--- src/components/app.js
+++ src/components/app.js
@@ -4,6 +4,8 @@ import Icons from "../helpers/icons";
 
 import Navbar from "./navBar/navBar";
 import Home from "./pages/home";
+import About from "./pages/aboutUs.js";
+
 import ContactUs from "./pages/contact";
 import GetQuote from "./pages/get-quote";
 import Footer from "./footer/footer";
@@ -22,6 +24,7 @@ export default class App extends Component {
           <Navbar />
           <Switch>
             <Route exact path="/" component={Home} />
+            <Route exact path="/about" component={About} />
             <Route path="/contact" component={ContactUs} />
             <Route path="/get-quote" component={GetQuote} />
           </Switch>
---
+++ src/components/pages/aboutUs.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+export default class About extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <h1> This is about us!</h1>
+      </div>
+    );
+  }
+}
