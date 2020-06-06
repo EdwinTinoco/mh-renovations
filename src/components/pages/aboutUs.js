59a9df101eb4666a63db5a8b9b867e28ddeb318c
Saul Vega <saulv@MacBook-Pro.local>
2020-06-06 00:13:18 -0600

    competed about us page

--- src/components/pages/aboutUs.js
+++ src/components/pages/aboutUs.js
@@ -1,23 +1,144 @@
 import React, { Component } from "react";
 import image1 from "../../../static/assets/images/aboutUs/aboutUs1.png";
+import profile1 from "../../../static/assets/images/aboutUs/profile1.png";
 export default class About extends Component {
   constructor(props) {
     super(props);
   }
   render() {
     return (
-      <div>
+      <div className="aboutUs-wrapper">
         <div id="aboutUs-header">
-          <img id="header-img" src={image1} alt="saw" />
-
-          <h1>Who We Are</h1>
+          <img src={image1} alt="saw" />
+          <div className="header-text-wrapper">
+            <h1>Who We Are</h1>
+          </div>
         </div>
         <div className="aboutUs-body">
           <h1>Established 2020</h1>
+          <h3>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Id eius
+            possimus dolorem architecto libero repellendus nulla quas veniam
+            illum sint.
+          </h3>
           <p>
-            M&H Renovations was established to offer all the services needed for
-            your next renovation all in one place.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit
+            consequatur corporis, nihil a velit ratione?
           </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum
+            quidem quod consequuntur accusantium neque, illo provident officiis
+            sunt non? Soluta?
+          </p>
+          <h1 className="line" />
+          <div className="about-profile">
+            <div className="avatar-image">
+              <img src={profile1} />
+            </div>
+            <div className="aboutDescription">
+              <h1>First and last name</h1>
+              <h3>Titles goes here</h3>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim
+                esse repellat necessitatibus sunt! Ea optio odio enim expedita
+                explicabo aliquam quae officia fugit voluptatum. Nemo pariatur
+                et dignissimos, ea modi rerum dolore, blanditiis nihil provident
+                dolor beatae obcaecati eos, animi quas explicabo! Quasi incidunt
+                laborum aliquid dignissimos, earum quos ab.
+              </p>
+            </div>
+          </div>
+          <h1 className="line" />
+          <div className="about-profile">
+            <div className="avatar-image">
+              <img src={profile1} />
+            </div>
+            <div className="aboutDescription">
+              <h1>First and last name</h1>
+              <h3>Titles goes here</h3>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim
+                esse repellat necessitatibus sunt! Ea optio odio enim expedita
+                explicabo aliquam quae officia fugit voluptatum. Nemo pariatur
+                et dignissimos, ea modi rerum dolore, blanditiis nihil provident
+                dolor beatae obcaecati eos, animi quas explicabo! Quasi incidunt
+                laborum aliquid dignissimos, earum quos ab.
+              </p>
+            </div>
+          </div>
+          <h1 className="line" />
+          <div className="about-profile">
+            <div className="avatar-image">
+              <img src={profile1} />
+            </div>
+            <div className="aboutDescription">
+              <h1>First and last name</h1>
+              <h3>Titles goes here</h3>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim
+                esse repellat necessitatibus sunt! Ea optio odio enim expedita
+                explicabo aliquam quae officia fugit voluptatum. Nemo pariatur
+                et dignissimos, ea modi rerum dolore, blanditiis nihil provident
+                dolor beatae obcaecati eos, animi quas explicabo! Quasi incidunt
+                laborum aliquid dignissimos, earum quos ab.
+              </p>
+            </div>
+          </div>
+          <h1 className="line" />
+          <div className="about-profile">
+            <div className="avatar-image">
+              <img src={profile1} />
+            </div>
+            <div className="aboutDescription">
+              <h1>First and last name</h1>
+              <h3>Titles goes here</h3>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim
+                esse repellat necessitatibus sunt! Ea optio odio enim expedita
+                explicabo aliquam quae officia fugit voluptatum. Nemo pariatur
+                et dignissimos, ea modi rerum dolore, blanditiis nihil provident
+                dolor beatae obcaecati eos, animi quas explicabo! Quasi incidunt
+                laborum aliquid dignissimos, earum quos ab.
+              </p>
+            </div>
+          </div>
+          <h1 className="line" />
+          <div className="about-profile">
+            <div className="avatar-image">
+              <img src={profile1} />
+            </div>
+            <div className="aboutDescription">
+              <h1>First and last name</h1>
+              <h3>Titles goes here</h3>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim
+                esse repellat necessitatibus sunt! Ea optio odio enim expedita
+                explicabo aliquam quae officia fugit voluptatum. Nemo pariatur
+                et dignissimos, ea modi rerum dolore, blanditiis nihil provident
+                dolor beatae obcaecati eos, animi quas explicabo! Quasi incidunt
+                laborum aliquid dignissimos, earum quos ab.
+              </p>
+            </div>
+          </div>
+          <h1 className="line" />
+          <div className="about-profile">
+            <div className="avatar-image">
+              <img src={profile1} />
+            </div>
+            <div className="aboutDescription">
+              <h1>First and last name</h1>
+              <h3>Titles goes here</h3>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim
+                esse repellat necessitatibus sunt! Ea optio odio enim expedita
+                explicabo aliquam quae officia fugit voluptatum. Nemo pariatur
+                et dignissimos, ea modi rerum dolore, blanditiis nihil provident
+                dolor beatae obcaecati eos, animi quas explicabo! Quasi incidunt
+                laborum aliquid dignissimos, earum quos ab.
+              </p>
+            </div>
+          </div>
+          <h1 className="line" />
         </div>
       </div>
     );
