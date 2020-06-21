3247bd8e21a5958ff58a603bb4795f292d6294a8
EdwinTinoco <jaredlotic@gmail.com>
2020-06-21 11:01:02 -0600

    added some more styles

--- src/components/pages/portfolio-hotels.js
+++ src/components/pages/portfolio-hotels.js
@@ -7,6 +7,13 @@ import Kitchen002 from "../kitchens/images/kitchen002.jpg"
 import Kitchen004 from "../kitchens/images/kitchen004.jpg"
 import Kitchen005 from "../kitchens/images/kitchen005.jpg"
 
+import ComfortInn from "../home/brands/comfort-inn.png"
+import HamptonInn from "../home/brands/hampton-inn.png"
+import HolidayInn from "../home/brands/hi.png"
+import HolidayInnEx from "../home/brands/holiday-inn-ex.png"
+import SleepInn from "../home/brands/sleep-inn.png"
+import SpringHill from "../home/brands/springhill.png"
+
 export default function PortfolioHotels() {
    return (
       <div className="portfolio-main-wrapper">
@@ -75,6 +82,32 @@ export default function PortfolioHotels() {
 
                <div className="read-more">
                   <Link to="/portfolio/hotel/common-areas">Common Areas</Link>
+               </div>
+            </div>
+         </div>
+
+         <div className="brands-wrapper">
+            <div className="title">
+               <h2>Some brands we've worked with...</h2>
+            </div>
+            <div className="brand-img-wrapper">
+               <div className="img">
+                  <img src={ComfortInn} />
+               </div>
+               <div className="img">
+                  <img src={HamptonInn} />
+               </div>
+               <div className="img">
+                  <img src={HolidayInn} />
+               </div>
+               <div className="img">
+                  <img src={HolidayInnEx} />
+               </div>
+               <div className="img">
+                  <img src={SleepInn} />
+               </div>
+               <div className="img">
+                  <img src={SpringHill} />
                </div>
             </div>
          </div>
