ae449c335bf42177d29e217ef4a61c717879bab2
EdwinTinoco <jaredlotic@gmail.com>
2020-06-22 07:52:14 -0600

    added lobby.js

--- src/components/portfolio/lobby.js
+++ src/components/portfolio/lobby.js
@@ -2,6 +2,9 @@ import React from "react"
 
 export default function PortfolioLobby() {
    return (
-      <h1>Portfolio Lobby</h1>
+      <div className="lobby-main-wrapper">
+         <h1>Portfolio Lobby</h1>
+
+      </div>
    )
 }
