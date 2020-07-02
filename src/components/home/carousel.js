b6dbdcc310ecaed9de14255683d54117910fcfe8
EdwinTinoco <jaredlotic@gmail.com>
2020-07-02 11:06:11 -0600

    fixed some bugs in carousel

--- src/components/home/carousel.js
+++ src/components/home/carousel.js
@@ -2,10 +2,11 @@ import React from "react";
 import { Carousel } from 'react-responsive-carousel';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 
-import Img1 from "./carousel_img/img1.jpg"
-import Img2 from "./carousel_img/img2.jpg"
-import Img3 from "./carousel_img/img3.jpg"
-import Img6 from "./carousel_img/img6.jpg"
+import Img1 from "./carousel_img/hie-heber-breakfast.jpg"
+import Img2 from "./carousel_img/hie-pool.jpg"
+import Img3 from "./carousel_img/hie-lobby.jpg"
+import Img4 from "./carousel_img/img1.jpg"
+import Img5 from "./carousel_img/lobby-2.jpg"
 
 export default function CarouselComponent() {
    return (
@@ -16,18 +17,24 @@ export default function CarouselComponent() {
             autoPlay
             showStatus={false}
             showThumbs={false}
+            interval={5000}
+            transitionTime={1800}
+            centerMode
          >
             <div>
-               <img src={Img1} />
+               <img src={Img3} />
             </div>
             <div>
                <img src={Img2} />
             </div>
             <div>
-               <img src={Img3} />
+               <img src={Img4} />
             </div>
             <div>
-               <img src={Img6} />
+               <img src={Img1} />
+            </div>
+            <div>
+               <img src={Img5} />
             </div>
 
          </Carousel>
